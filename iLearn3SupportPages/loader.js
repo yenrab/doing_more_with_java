21b1c523140d37f06efa946a3b1649c66c8d0bfc
yenrab <barney.cit@gmail.com>
2016-04-08 10:05:15 -0600

    finished splitting the syllabus and made other minor changes

--- iLearn3SupportPages/loader.js
+++ iLearn3SupportPages/loader.js
@@ -1,21 +1,25 @@
 function load(toLoad, customDisplayLocation, isRemoteRequst, callBack){
-			var dataRequester = new XMLHttpRequest()
-			dataRequester.onreadystatechange = function() {
-			  if (dataRequester.readyState == 4 && dataRequester.status == 200) {
-			  	var displayLocation = "barneyMain"
-			  	if (customDisplayLocation){
-			  		displayLocation = customDisplayLocation
-			  	}
-			    document.getElementById(displayLocation).innerHTML = dataRequester.responseText;
-			    if(callBack){
-			    	callBack()
-			    }
-			  }
-			}
-            var prefix = ""
-            if(isRemoteRequst){
-                prefix = "https://yenrab.github.io/doing_more_with_java/iLearn3SupportPages/"
-            }
-			dataRequester.open("GET", prefix+toLoad+".html", true);
-			dataRequester.send();
-		}
+	var dataRequester = new XMLHttpRequest()
+	dataRequester.onreadystatechange = function() {
+	  if (dataRequester.readyState == 4 && dataRequester.status == 200) {
+	  	var displayLocation = "barneyMain"
+	  	if (customDisplayLocation){
+	  		displayLocation = customDisplayLocation
+	  	}
+	    document.getElementById(displayLocation).innerHTML = dataRequester.responseText;
+	    if(callBack){
+	    	callBack()
+	    }
+	  }
+	}
+    var prefix = ""
+    if(isRemoteRequst){
+        prefix = "https://yenrab.github.io/doing_more_with_java/iLearn3SupportPages/"
+    }
+	dataRequester.open("GET", prefix+toLoad+".html", true);
+	dataRequester.send();
+}
+function stopDefault(theEvent){
+	theEvent.preventDefault()
+	return false
+}
